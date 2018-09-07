802c8a4cfff03409164838d1cbb2ebef7e0fb877
Stephen Gordon <gordoste@iinet.net.au>
2018-09-07 20:12:02 +1000

    Make DB location relative to app path

--- MoonServer/Controllers/BaseController.cs
+++ MoonServer/Controllers/BaseController.cs
@@ -1,6 +1,4 @@
 ﻿using MoonServer.Models;
-using System;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MoonServer.Controllers
@@ -9,16 +7,5 @@ namespace MoonServer.Controllers
     {
         protected MoonServerDB db = new MoonServerDB();
 
-        // Add a dependency on a magic CacheKey for every request
-        protected override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            filterContext.HttpContext.Response.AddCacheItemDependency(Constants.CacheKey);
-        }
-
-        // Update the magic cache key so that cached results are invalid
-        protected void InvalidateCache()
-        {
-            HttpRuntime.Cache.Insert(Constants.CacheKey, DateTime.Now);
-        }
     }
 }
--- MoonServer/Global.asax.cs
+++ MoonServer/Global.asax.cs
@@ -1,6 +1,4 @@
-﻿using MoonServer.Models;
-using System;
-using System.Linq;
+﻿using System;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -13,13 +11,6 @@ namespace MoonServer
     {
         protected void Application_Start()
         {
-            // Initialise the cache entry
-            HttpRuntime.Cache.Insert(Constants.CacheKey, DateTime.Now);
-            using (MoonServerDB db = new MoonServerDB())
-            {
-                HttpRuntime.Cache.Insert(Constants.GradeKey, db.Grades.ToList().ConvertAll(g => g.AmericanName));
-            }
-
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
