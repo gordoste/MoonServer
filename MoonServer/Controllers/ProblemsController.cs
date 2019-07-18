25da4ee37f1b8704b8aa4b177aeb4379962c34bc
gordoste <gordoste@iinet.net.au>
2019-07-18 22:37:01 +1000

    Controller passes problem lists to view

--- MoonServer/Controllers/ProblemsController.cs
+++ MoonServer/Controllers/ProblemsController.cs
@@ -1,10 +1,5 @@
-﻿using MoonServer.Models;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
+﻿using System.Data.Entity;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MoonServer.Controllers
@@ -12,12 +7,13 @@ namespace MoonServer.Controllers
     public class ProblemsController : BaseController
     {
         // GET: Problems
-        //public async Task<ActionResult> Index()
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
+        //public ActionResult Index()
         {
             //    var problems = db.Problems.Include(p => p.Grade).Include(p => p.HoldSetup);
             //    return View(await problems.ToListAsync());
-            return View(new List<Problem>());
+            var problemLists = db.ProblemLists;
+            return View(await problemLists.ToListAsync());
         }
     }
 }
