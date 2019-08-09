df64f3246b598119ec050e984b11b904f3f5629f
gordoste <gordoste@iinet.net.au>
2019-08-09 22:35:43 +1000

    Add program to export problems to filesystem

---
+++ ProblemExport/ProblemExportForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ProblemExport
+{
+    partial class ProblemExportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exportBtn = new System.Windows.Forms.Button();
+            this.folderDlg = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // exportBtn
+            // 
+            this.exportBtn.Location = new System.Drawing.Point(57, 41);
+            this.exportBtn.Name = "exportBtn";
+            this.exportBtn.Size = new System.Drawing.Size(75, 23);
+            this.exportBtn.TabIndex = 0;
+            this.exportBtn.Text = "Export";
+            this.exportBtn.UseVisualStyleBackColor = true;
+            this.exportBtn.Click += new System.EventHandler(this.ExportBtn_Click);
+            // 
+            // ProblemExportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 107);
+            this.Controls.Add(this.exportBtn);
+            this.Name = "ProblemExportForm";
+            this.Text = "Problem Exporter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exportBtn;
+        private System.Windows.Forms.FolderBrowserDialog folderDlg;
+    }
+}
+
---
+++ ProblemExport/ProblemExportForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using MoonServer.Models;
+
+namespace ProblemExport
+{
+    public partial class ProblemExportForm : Form
+    {
+        private MoonServerDB moonServer = new MoonServerDB();
+
+        public ProblemExportForm()
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", "C:\\Users\\gordo\\source\\repos\\MoonServer\\MoonServer\\App_Data");
+            InitializeComponent();
+        }
+
+        private void ExportBtn_Click(object sender, EventArgs e)
+        {
+            if (folderDlg.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            foreach (Problem p in moonServer.Problems)
+            {
+                File.WriteAllText(folderDlg.SelectedPath + "\\" + p.MoonID + ".dat",
+                    String.Join(",", p.ProblemPositions.ToList().ConvertAll(pp => pp.Position.Name)));
+            }
+        }
+    }
+}
